e961b115cc890ac5595a975d40367fdbea4ea0ac
Fanie Duvenage <duvenage911@gmail.com>
2025-07-17 18:52:05 +0200

    Update Init Commit

--- src/SQLCon.cs
+++ src/SQLCon.cs
@@ -2,6 +2,6 @@
 
 public class SQLCon
 {
-    //public string connectionString = "Server=FANIE-DELLXPS13\\ABMS_SQL_SVR;Database=TestDB;User Id=sa;Password=Tester@123;TrustServerCertificate=true;";
-    public string connectionString = "Server=FANIE-F15\\ABMS_SQL;Database=TestDB;User Id=sa;Password=Tester@123;TrustServerCertificate=true;";
+    public string connectionString = "Server=FANIE-DELLXPS13\\ABMS_SQL_SVR;Database=TestDB;User Id=sa;Password=Tester@123;TrustServerCertificate=true;";
+    //public string connectionString = "Server=FANIE-F15\\ABMS_SQL;Database=TestDB;User Id=sa;Password=Tester@123;TrustServerCertificate=true;";
 }
